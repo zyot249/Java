a85d68712333a8a3c0f414e2a8bd997a85abdbac
zyot249 <windnd249@gmail.com>
2019-02-08 13:02:06 +0700

    first java of the new year

---
+++ HelloWorld/src/SayHello.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class SayHello {
+    public static void main(String args[]){
+        JOptionPane.showMessageDialog(null,"Welcome to my world!!\n And happy new year!","Say Hello",JOptionPane.INFORMATION_MESSAGE);
+    }
+}
