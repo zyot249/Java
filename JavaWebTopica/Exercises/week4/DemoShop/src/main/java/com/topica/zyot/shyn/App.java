9c14893cd346dfb0c9b24975d830c8f7107796f3
zyot249 <windnd249@gmail.com>
2019-07-16 21:27:26 +0700

    update

--- JavaWebTopica/Exercises/week4/DemoShop/src/main/java/com/topica/zyot/shyn/App.java
+++ JavaWebTopica/Exercises/week4/DemoShop/src/main/java/com/topica/zyot/shyn/App.java
@@ -1,5 +1,6 @@
 package com.topica.zyot.shyn;
 
+
 import java.sql.*;
 
 public class App {
@@ -12,74 +13,50 @@ public class App {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            saveOrder("ORDER004", 1, 5, connection);
-            connection.close();
+            saveOrder("ORDER006", 1, 500, connection);
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
     private static void saveOrder(String orderCode, int prodId, int quantity, Connection connection) {
-        PreparedStatement statement = null;
         Savepoint savepoint;
         try {
             connection.setAutoCommit(false);
             // insert order to order of customer
-            String insertOrderCus = "INSERT INTO DemoShop.order_from_customer(id, prod_id, quantity, success)" +
-                    " VALUES (?, ?, ?, true);";
-            statement = connection.prepareStatement(insertOrderCus);
-            statement.setString(1, orderCode);
-            statement.setInt(2, prodId);
-            statement.setInt(3, quantity);
-            statement.executeUpdate();
-
+            insertOrderCus(orderCode, prodId, quantity, connection);
             // savepoint
             savepoint = connection.setSavepoint("save_to_order_cus");
-
             // insert order to order
-            String insertOrder = "INSERT INTO DemoShop.order(id ,prod_id, quantity) VALUES (?, ?, ?);";
-            statement = connection.prepareStatement(insertOrder);
-            statement.setString(1, orderCode);
-            statement.setInt(2, prodId);
-            statement.setInt(3, quantity);
-            statement.executeUpdate();
-
+            insertOrder(orderCode, prodId, quantity, connection);
             // get quantity in stock
-            String getQuanInStock = "SELECT quan_in_stock FROM DemoShop.inventory WHERE prod_id = ?";
-            statement = connection.prepareStatement(getQuanInStock);
-            statement.setInt(1, prodId);
-            ResultSet resultSet = statement.executeQuery();
-            resultSet.next();
-            int quanInStock = resultSet.getInt("quan_in_stock");
-
+            int quanInStock = getQuantityInStock(prodId, connection);
+            if (quanInStock < 0) {
+                System.out.println("Cannot get quantity of product in stock!");
+                return;
+            }
             // check if quantity <= quan_in_stock
             if (quantity <= quanInStock) {
                 // if quantity in stock is enough --> order success and update inventory
                 // update quan_in_stock
-                String updateQuanInStock = "UPDATE DemoShop.inventory SET quan_in_stock = quan_in_stock - ? WHERE prod_id = ?";
-                statement = connection.prepareStatement(updateQuanInStock);
-                statement.setInt(1, quantity);
-                statement.setInt(2, prodId);
-                statement.executeUpdate();
-
+                updateQuanInStock(prodId, quantity, connection);
                 // update sales
-                String updateSales = "UPDATE DemoShop.inventory SET sales = sales + ? WHERE prod_id = ?";
-                statement = connection.prepareStatement(updateSales);
-                statement.setInt(1, quantity);
-                statement.setInt(2, prodId);
-                statement.executeUpdate();
-
+                updateSales(prodId, quantity, connection);
                 // commit transaction
                 connection.commit();
                 System.out.println("Customer orders successfully!");
             } else {
                 // if quantity in stock is not enough --> order fail --> roll back and update success of order of customer
                 connection.rollback(savepoint);
-                String updateOrderFail = "UPDATE DemoShop.order_from_customer SET success = false WHERE id = ?;";
-                statement = connection.prepareStatement(updateOrderFail);
-                statement.setString(1, orderCode);
-                statement.executeUpdate();
-
+                setOrderCusFail(orderCode, connection);
                 // commit
                 connection.commit();
                 System.out.println("Customer orders failed!");
@@ -93,13 +70,64 @@ public class App {
             } catch (SQLException ex) {
                 ex.printStackTrace();
             }
-        } finally {
-            if (statement != null)
-                try {
-                    statement.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
         }
+    }
+
+    private static void insertOrderCus(String orderCode, int prodId, int quantity, Connection connection) throws SQLException {
+        String insertOrderCus = "INSERT INTO DemoShop.order_from_customer(id, prod_id, quantity, success) VALUES (?, ?, ?, true);";
+        PreparedStatement statement = connection.prepareStatement(insertOrderCus);
+        statement.setString(1, orderCode);
+        statement.setInt(2, prodId);
+        statement.setInt(3, quantity);
+        statement.executeUpdate();
+        statement.close();
+    }
+
+    private static void insertOrder(String orderCode, int prodId, int quantity, Connection connection) throws SQLException {
+        String insertOrder = "INSERT INTO DemoShop.order(id ,prod_id, quantity) VALUES (?, ?, ?);";
+        PreparedStatement statement = connection.prepareStatement(insertOrder);
+        statement.setString(1, orderCode);
+        statement.setInt(2, prodId);
+        statement.setInt(3, quantity);
+        statement.executeUpdate();
+        statement.close();
+    }
+
+    private static int getQuantityInStock(int prodId, Connection connection) throws SQLException {
+        String getQuanInStock = "SELECT quan_in_stock FROM DemoShop.inventory WHERE prod_id = ?";
+        PreparedStatement statement = connection.prepareStatement(getQuanInStock);
+        statement.setInt(1, prodId);
+        ResultSet resultSet = statement.executeQuery();
+        int quanInStock = -1;
+        if (resultSet.next())
+            quanInStock = resultSet.getInt("quan_in_stock");
+        statement.close();
+        return quanInStock;
+    }
+
+    private static void updateQuanInStock(int prodId, int quantity, Connection connection) throws SQLException {
+        String updateQuanInStock = "UPDATE DemoShop.inventory SET quan_in_stock = quan_in_stock - ? WHERE prod_id = ?";
+        PreparedStatement statement = connection.prepareStatement(updateQuanInStock);
+        statement.setInt(1, quantity);
+        statement.setInt(2, prodId);
+        statement.executeUpdate();
+        statement.close();
+    }
+
+    private static void updateSales(int prodId, int quantity, Connection connection) throws SQLException {
+        String updateSales = "UPDATE DemoShop.inventory SET sales = sales + ? WHERE prod_id = ?";
+        PreparedStatement statement = connection.prepareStatement(updateSales);
+        statement.setInt(1, quantity);
+        statement.setInt(2, prodId);
+        statement.executeUpdate();
+        statement.close();
+    }
+
+    private static void setOrderCusFail(String orderCode, Connection connection) throws SQLException {
+        String updateOrderFail = "UPDATE DemoShop.order_from_customer SET success = false WHERE id = ?;";
+        PreparedStatement statement = connection.prepareStatement(updateOrderFail);
+        statement.setString(1, orderCode);
+        statement.executeUpdate();
+        statement.close();
     }
 }
