112c665ed15cbef46b393bf85c83b3ccf255f8fe
zyot249 <windnd249@gmail.com>
2019-07-16 21:28:08 +0700

    update file query.sql

--- JavaWebTopica/Exercises/week4/FirstConnectionToMySQL/query.sql
+++ JavaWebTopica/Exercises/week4/FirstConnectionToMySQL/query.sql
@@ -6,3 +6,14 @@ END; $$
 DELIMITER ;
 
 CALL GetAllStudents();
+
+DROP PROCEDURE GetAllStudents;
+DELIMITER $$
+DROP PROCEDURE IF EXISTS GetAllStudentsByClass$$
+CREATE PROCEDURE GetAllStudentsByClass(in_clazz varchar(45))
+BEGIN
+    SELECT * FROM student WHERE student.clazz = in_clazz;
+END; $$
+DELIMITER ;
+
+CALL GetAllStudentsByClass('ICT02-K61');
--- JavaWebTopica/Exercises/week4/FirstConnectionToMySQL/src/main/java/com/topica/zyot/shyn/App.java
+++ JavaWebTopica/Exercises/week4/FirstConnectionToMySQL/src/main/java/com/topica/zyot/shyn/App.java
@@ -8,21 +8,42 @@ public class App {
     private static final String PASSWORD = "conkuncon249";
 
     public static void main(String[] args) {
-        Connection connection;
+        Connection connection = null;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            getAllStudentsByCallableStatement(connection);
-            connection.close();
+//            getAllStudents(connection);
+            getAllStudentsByClass("ICT02-K61", connection);
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
+        } finally {
+            if (connection != null)
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
         }
     }
 
-    private static void getAllStudentsByCallableStatement(Connection connection) throws SQLException {
+    private static void getAllStudents(Connection connection) throws SQLException {
         String query = "{call GetAllStudents()}";
         CallableStatement callableStatement = connection.prepareCall(query);
         ResultSet resultSet = callableStatement.executeQuery();
+        printStudentTable(resultSet);
+        callableStatement.close();
+    }
+
+    private static void getAllStudentsByClass(String className, Connection connection) throws SQLException {
+        String query = "{call GetAllStudentsByClass(?)}";
+        CallableStatement callableStatement = connection.prepareCall(query);
+        callableStatement.setString(1, className);
+        ResultSet resultSet = callableStatement.executeQuery();
+        printStudentTable(resultSet);
+        callableStatement.close();
+    }
+
+    private static void printStudentTable(ResultSet resultSet) throws SQLException {
         System.out.println("---------------------RESULT---------------------");
         while (resultSet.next()) {
             int id = resultSet.getInt("id");
@@ -31,6 +52,5 @@ public class App {
             String clazz = resultSet.getString("clazz");
             System.out.printf("id: %-4s \tfirstname: %-12s \tlastname: %-22s \tclass: %s\n", id, firstname, lastname, clazz);
         }
-        callableStatement.close();
     }
 }
