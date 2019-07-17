53613946b96b4bbe8ee505514c338b98aaea81ba
zyot249 <windnd249@gmail.com>
2019-07-17 16:27:08 +0700

    update

--- JavaWebTopica/Exercises/week4/DemoShop/query.sql
+++ JavaWebTopica/Exercises/week4/DemoShop/query.sql
@@ -9,13 +9,13 @@ BEGIN
         SET autocommit = false;
         INSERT INTO DemoShop.order_from_customer(id, prod_id, quantity, success)
         VALUES (in_order_code, in_pro_id, in_quantity, true);
-        SAVEPOINT A;
+        SAVEPOINT save_to_order_cus;
         INSERT INTO DemoShop.order(id, prod_id, quantity) VALUES (in_order_code, in_pro_id, in_quantity);
         IF in_quantity <= (SELECT quan_in_stock FROM DemoShop.inventory WHERE prod_id = in_pro_id) THEN
             UPDATE DemoShop.inventory SET quan_in_stock = quan_in_stock - in_quantity WHERE prod_id = in_pro_id;
             UPDATE DemoShop.inventory SET sales = sales + in_quantity WHERE prod_id = in_pro_id;
         ELSE
-            ROLLBACK TO A;
+            ROLLBACK TO save_to_order_cus;
             UPDATE DemoShop.order_from_customer SET success = false WHERE id = in_order_code;
             COMMIT;
         END IF;
